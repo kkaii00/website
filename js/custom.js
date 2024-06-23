722517eb7d122934f0c763feefe46d64263a055d
kkaii00 <gkaiieng77201@gmail.com>
2024-06-24 07:55:24 +0800

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -17,21 +17,7 @@ $(document).ready(function(){
 5. welcome animation support
 ======================================*/
 
-    // 1. Scroll To Top 
-		$(window).on('scroll',function () {
-			if ($(this).scrollTop() > 600) {
-				$('.return-to-top').fadeIn();
-			} else {
-				$('.return-to-top').fadeOut();
-			}
-		});
-		$('.return-to-top').on('click',function(){
-				$('html, body').animate({
-				scrollTop: 0
-			}, 1500);
-			return false;
-		});
-	
+
 	
 	
 	// 2. Smooth Scroll spy
